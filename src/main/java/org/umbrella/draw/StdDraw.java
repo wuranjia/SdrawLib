b15071bcd119fe45825239ab8ec40b33bc846bbb
ranjia <junstin001@gmail.com>
2017-06-03 22:36:53 +0800

    'modify'

--- src/main/java/org/umbrella/draw/StdDraw.java
+++ src/main/java/org/umbrella/draw/StdDraw.java
@@ -159,7 +159,6 @@ public final class StdDraw implements ActionListener, MouseListener, MouseMotion
      *
      * @param w the width as a number of pixels
      * @param h the height as a number of pixels
-     * @throws a IllegalArgumentException if the width or height is 0 or negative
      */
     public static void setCanvasSize(int w, int h) {
         if (w < 1 || h < 1) throw new IllegalArgumentException("width and height must be positive");
